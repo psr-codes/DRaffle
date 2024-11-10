cd9a19fd106d1ba12c78cdc39260e40cb5e91dc7
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-11-10 23:22:51 +0530

    Remove deleted files

--- client-2/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
-import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
-export default function Home() {
-    <div>
-        <WalletSelector />
-    </div>;
-}

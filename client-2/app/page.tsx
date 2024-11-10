cebd35fb3957ffc5f5b05736f73e2e9e83c1df1f
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-11-10 19:15:14 +0530

    client in making

---
+++ client-2/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+export default function Home() {
+    <div>
+        <WalletSelector />
+    </div>;
+}
---
+++ client-3/src/App.tsx
@@ -0,0 +1,12 @@
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+
+function App() {
+    return (
+        <div>
+            <WalletSelector />
+        </div>
+    );
+}
+
+export default App;
